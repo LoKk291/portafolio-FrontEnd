944bef18ae66fd1f0664674cfb6b065a73a8fea8
LoKk291 <66387374+LoKk291@users.noreply.github.com>
2023-01-26 14:29:36 -0300

    -

--- FrontEnd/src/app/components/header/header.component.ts
+++ FrontEnd/src/app/components/header/header.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-
+  constructor(){
+    console.log('Componente "header" cargado')
+  }
 }
