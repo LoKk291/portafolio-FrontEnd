85f5197e7e2b460992b6c0222b246e6c678afb73
LoKk291 <66387374+LoKk291@users.noreply.github.com>
2023-01-26 14:25:41 -0300

    Se agrega un componente de prueba

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { PruebaComponent } from './components/prueba/prueba.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    PruebaComponent
   ],
   imports: [
     BrowserModule,
