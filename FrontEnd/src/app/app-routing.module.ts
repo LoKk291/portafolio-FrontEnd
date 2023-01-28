4e2cd2164b30ca979631225cf2c3ba6fae4d0371
LoKk291 <66387374+LoKk291@users.noreply.github.com>
2023-01-28 20:48:57 -0300

    Se agregan los enrutamientos restantes

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AcercaDeMiComponent } from './components/acerca-de-mi/acerca-de-mi.component';
+import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponent } from './components/home/home.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
 
 const routes: Routes = [
   {path: '', component:HomeComponent},
+  {path: 'home', component:HomeComponent},
+  {path: 'acerca-de-mi', component:AcercaDeMiComponent},
+  {path: 'proyectos', component:ProyectsComponent},
+  {path: 'contact', component:ContactComponent}
 
 ];
 
