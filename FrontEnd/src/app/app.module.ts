97f098c8f278a3f009182681d2c0f49c5ce3fd85
LoKk291 <66387374+LoKk291@users.noreply.github.com>
2023-01-28 20:01:55 -0300

    Eliminación del componente header
    Este compnente estaba en la ruta equivocada

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -4,14 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { HeaderComponent } from './header/header.component';
 import { AcercaDeMiComponent } from './components/acerca-de-mi/acerca-de-mi.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HeaderComponent,
     AcercaDeMiComponent
   ],
   imports: [
