b3fe2892c5699865ec27cab5ba8618637b728c95
LoKk291 <66387374+LoKk291@users.noreply.github.com>
2023-01-28 20:45:47 -0300

    Creación del componente poyects

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { AcercaDeMiComponent } from './components/acerca-de-mi/acerca-de-mi.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ProyectsComponent } from './components/proyects/proyects.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     AcercaDeMiComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProyectsComponent
   ],
   imports: [
     BrowserModule,
