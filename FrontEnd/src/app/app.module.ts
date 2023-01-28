c41faf9e1e92f1e4866d0b919650287bb05cf584
LoKk291 <66387374+LoKk291@users.noreply.github.com>
2023-01-28 18:16:28 -0300

    Eliminación componente header
    Se elimina ya que en un principio este componente iba a contener la barra de navegación de la página, pero esta debe estar en el componente principal, por ende el componente header no es necesario

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
 import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
     HomeComponent
   ],
   imports: [
